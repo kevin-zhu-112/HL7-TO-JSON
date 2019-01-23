7f9e72b2ca012ab5ca5622f8a38e09e70b2acb77
haidangdam <JDzqt995>
2019-01-23 15:10:42 -0500

    Connect with MongoDB

--- asonconverter/src/test/java/org/cdc/jasonconverter/asonconverter/Tester.java
+++ asonconverter/src/test/java/org/cdc/jasonconverter/asonconverter/Tester.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 import org.json.JSONObject;
 
 import gov.cdc.ncezid.eip.services.transform.helper.HL7Helper;
@@ -23,11 +26,15 @@ public class Tester {
 			helper.parseToJSON(validMessage);
 			*/
 			StringTokenizer token = new StringTokenizer("");
-			String content = new Scanner(new File("./Project Test Files\\2.5.1_GPS_Maxiumum Data Complex Microbiology.txt-revHEAD.svn000.tmp.txt")).useDelimiter("\\Z").next();
+			String content = new Scanner(new File("./Project Test Files/2.5.1_GPS_Maxiumum Data Complex Microbiology.txt-revHEAD.svn000.tmp.txt")).useDelimiter("\\Z").next();
 			//String content = validMessage;
 			 System.out.println(content);
 			HL7Helper helper = HL7Helper.getInstance();
 			JSONObject object=helper.parseSingleMessageToJSON(content);
+			MongoClient client = new MongoClient( "localhost" , 27017 );
+			MongoCollection<Document> collection = client.getDatabase("mydb").getCollection("test");
+			Document doc = Document.parse(object.toString());
+			collection.insertOne(doc);
 			System.out.println(object);
 		} catch (IOException e) {
 			System.out.println("error thrown:" + e);
